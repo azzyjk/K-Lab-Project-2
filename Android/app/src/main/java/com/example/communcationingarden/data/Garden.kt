e961a710fe3137011091a1d7d2b26572556af87a
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-10-24 05:31:16 +0900

    feat: Garden priceText 추가

--- Android/app/src/main/java/com/example/communcationingarden/data/Garden.kt
+++ Android/app/src/main/java/com/example/communcationingarden/data/Garden.kt
@@ -1,6 +1,11 @@
 package com.example.communcationingarden.data
 
+import com.example.communcationingarden.util.toDistanceText
+
 data class Garden(
 	val name: String,
 	val distance: Double
-)
+) {
+
+	val distanceText = distance.toDistanceText()
+}
---
+++ Android/app/src/main/java/com/example/communcationingarden/util/Util.kt
@@ -0,0 +1,5 @@
+package com.example.communcationingarden.util
+
+fun Double.toDistanceText(): String {
+	return "${this}Km"
+}
