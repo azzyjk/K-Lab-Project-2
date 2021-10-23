d7eaccdfac4d5becc5cad1155e31121bb7b32f1a
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-10-24 05:33:37 +0900

    feat(GardenListAdapter): RecyclerView onClickListener 추가

--- Android/app/src/main/java/com/example/communcationingarden/adapter/GardenListAdapter.kt
+++ Android/app/src/main/java/com/example/communcationingarden/adapter/GardenListAdapter.kt
@@ -6,22 +6,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.communcationingarden.data.Garden
 import com.example.communcationingarden.databinding.ItemGardenListBinding
 
-class GardenListAdapter : RecyclerView.Adapter<GardenListAdapter.GardenViewHolder>() {
+class GardenListAdapter(private val onClick: () -> Unit) :
+	RecyclerView.Adapter<GardenListAdapter.GardenViewHolder>() {
 
 	private var gardenList: List<Garden> = listOf()
 
-	class GardenViewHolder(private val binding: ItemGardenListBinding) :
+	class GardenViewHolder(
+		private val binding: ItemGardenListBinding,
+		private val onClick: () -> Unit
+	) :
 		RecyclerView.ViewHolder(binding.root) {
+
+		init {
+			binding.root.setOnClickListener {
+				binding.garden?.let {
+					onClick()
+				}
+			}
+		}
 
 		fun setup(garden: Garden) {
 			binding.garden = garden
+			binding.executePendingBindings()
 		}
 	}
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GardenViewHolder {
 		val binding =
 			ItemGardenListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-		return GardenViewHolder(binding)
+		return GardenViewHolder(binding, onClick)
 	}
 
 	override fun onBindViewHolder(holder: GardenViewHolder, position: Int) {
@@ -32,5 +45,6 @@ class GardenListAdapter : RecyclerView.Adapter<GardenListAdapter.GardenViewHolde
 
 	fun updateGardenList(newGardenList: List<Garden>) {
 		gardenList = newGardenList
+		notifyDataSetChanged()
 	}
 }
