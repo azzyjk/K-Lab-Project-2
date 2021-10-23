32d86163bfba63109429fd4e7d25d675eb13323d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-10-24 04:15:09 +0900

    feat: SignupFragment Login으로 돌아가는 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/signup/SignupFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/signup/SignupFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.communcationingarden.EventObserver
+import com.example.communcationingarden.R
 import com.example.communcationingarden.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
@@ -13,13 +17,28 @@ class SignupFragment : Fragment() {
 	private var _binding: FragmentSignupBinding? = null
 	private val binding get() = _binding!!
 	private val signupViewModel: SignupViewModel by viewModels()
+	private lateinit var navigationController: NavController
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
-		_binding = FragmentSignupBinding.inflate(layoutInflater, container, false)
+		_binding = FragmentSignupBinding.inflate(layoutInflater, container, false).apply {
+			viewModel = signupViewModel
+		}
 		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		navigationController = findNavController()
+		initObserve()
+	}
+
+	private fun initObserve() = with(signupViewModel) {
+		loginScreenEvent.observe(viewLifecycleOwner, EventObserver {
+			navigationController.navigate(R.id.loginFragment)
+		})
 	}
 
 	override fun onDestroyView() {
--- Android/app/src/main/java/com/example/communcationingarden/signup/SignupViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/signup/SignupViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.communcationingarden.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.communcationingarden.Event
 
 class SignupViewModel : ViewModel() {
+
+	private val _loginScreenEvent = MutableLiveData<Event<Unit>>()
+	val loginScreenEvent: LiveData<Event<Unit>> = _loginScreenEvent
+
+	fun openLoginScreen() {
+		println("Hello")
+		_loginScreenEvent.value = Event(Unit)
+	}
 }
