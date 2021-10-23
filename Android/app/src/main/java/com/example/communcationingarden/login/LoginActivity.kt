f88be0b09aa126db5c04e2f97fcd3b0b7c865617
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-10-24 02:55:23 +0900

    feat: Login에서 Main으로 가는 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.communcationingarden.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.communcationingarden.EventObserver
+import com.example.communcationingarden.MainActivity
 import com.example.communcationingarden.databinding.ActivityLoginBinding
 import com.example.communcationingarden.login.model.LoginViewModel
 
@@ -13,7 +16,21 @@ class LoginActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		binding = ActivityLoginBinding.inflate(layoutInflater)
+		binding = ActivityLoginBinding.inflate(layoutInflater).apply {
+			viewModel = loginViewModel
+		}
 		setContentView(binding.root)
+		initObserve()
+	}
+
+	private fun initObserve() = with(loginViewModel) {
+		mainScreenEvent.observe(this@LoginActivity, EventObserver {
+			startMainScreen()
+		})
+	}
+
+	private fun startMainScreen() {
+		val intent = Intent(this, MainActivity::class.java)
+		startActivity(intent)
 	}
 }
--- Android/app/src/main/java/com/example/communcationingarden/login/LoginViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.communcationingarden.login.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.communcationingarden.Event
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
+
+	private val _mainScreenEvent = MutableLiveData<Event<Unit>>()
+	val mainScreenEvent: LiveData<Event<Unit>> = _mainScreenEvent
+
+	fun openMainScreen() {
+		_mainScreenEvent.value = Event(Unit)
+	}
 }
