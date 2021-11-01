5da5edf9ca7445bf44e2bcdb24eac25ce6085fdc
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-01 23:27:30 +0900

    feat(SelectViewModel): User Position 라이브 데이터 추가

--- Android/app/src/main/java/com/example/communcationingarden/select/SelectViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/select/SelectViewModel.kt
@@ -4,13 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.communcationingarden.Event
+import com.example.communcationingarden.data.Position
 
 class SelectViewModel : ViewModel() {
 
 	private val _mainScreenEvent = MutableLiveData<Event<Unit>>()
 	val mainScreenEvent: LiveData<Event<Unit>> = _mainScreenEvent
+	private var _userPositionLiveData = Position()
+	val userPositionLiveData get() = _userPositionLiveData
 
 	fun openMainScreen() {
 		_mainScreenEvent.value = Event(Unit)
+	}
+
+	fun setUserPosition(position: Position) {
+		_userPositionLiveData = position
 	}
 }
