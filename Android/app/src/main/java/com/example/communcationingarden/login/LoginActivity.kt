f2aae08927207814b844b73faf4bb1d0e4171342
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-01 23:28:53 +0900

    feat: Activity 이동 시 유저 위치 전송

--- Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
@@ -1,17 +1,25 @@
 package com.example.communcationingarden.login
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.communcationingarden.EventObserver
+import com.example.communcationingarden.data.Position
 import com.example.communcationingarden.databinding.ActivityLoginBinding
 import com.example.communcationingarden.info.MainActivity
 import com.example.communcationingarden.select.SelectViewModel
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class LoginActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityLoginBinding
+	private lateinit var locationManager: LocationManager
 	private val selectViewModel: SelectViewModel by viewModels()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +27,12 @@ class LoginActivity : AppCompatActivity() {
 		binding = ActivityLoginBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		initObserve()
+		initView()
+		getUserCurrentLocation()
+	}
+
+	private fun initView() {
+		locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 	}
 
 	private fun initObserve() = with(selectViewModel) {
@@ -28,7 +42,34 @@ class LoginActivity : AppCompatActivity() {
 	}
 
 	private fun startMainScreen() {
-		val intent = Intent(this, MainActivity::class.java)
+		val userPositionJson = Json.encodeToString(selectViewModel.userPositionLiveData)
+		val intent = Intent(this, MainActivity::class.java).apply {
+			putExtra("Position", userPositionJson)
+		}
 		startActivity(intent)
+	}
+
+	private fun getUserCurrentLocation() {
+		checkLocationPermission()
+		val userLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+		userLocation?.let { location ->
+			val latitude = location.latitude
+			val longitude = location.longitude
+			selectViewModel.setUserPosition(Position(latitude, longitude))
+		}
+	}
+
+	private fun checkLocationPermission() {
+		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			requestPermissions(
+				arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+				LOCATION_PERMISSION_REQUEST_CODE
+			)
+		}
+	}
+
+	companion object {
+
+		const val LOCATION_PERMISSION_REQUEST_CODE = 100
 	}
 }
