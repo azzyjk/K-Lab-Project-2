7832e1e585c31ec5e9a580adc6f9c7496047773d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-04 05:37:52 +0900

    refactor: 화면 이동하는 코드 fragment로 이동

--- Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
@@ -1,75 +1,19 @@
 package com.example.communcationingarden.login
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.communcationingarden.EventObserver
-import com.example.communcationingarden.data.Position
 import com.example.communcationingarden.databinding.ActivityLoginBinding
-import com.example.communcationingarden.info.InfoActivity
-import com.example.communcationingarden.select.SelectViewModel
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 class LoginActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityLoginBinding
 	private lateinit var locationManager: LocationManager
-	private val selectViewModel: SelectViewModel by viewModels()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityLoginBinding.inflate(layoutInflater)
 		setContentView(binding.root)
-		initObserve()
-		initView()
-		getUserCurrentLocation()
-	}
-
-	private fun initView() {
-		locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-	}
-
-	private fun initObserve() = with(selectViewModel) {
-		mainScreenEvent.observe(this@LoginActivity, EventObserver {
-			startMainScreen()
-		})
-	}
-
-	private fun startMainScreen() {
-		val userPositionJson = Json.encodeToString(selectViewModel.userPositionLiveData)
-		val intent = Intent(this, InfoActivity::class.java).apply {
-			putExtra("Position", userPositionJson)
-		}
-		startActivity(intent)
-	}
-
-	private fun getUserCurrentLocation() {
-		checkLocationPermission()
-		val userLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-		userLocation?.let { location ->
-			val latitude = location.latitude
-			val longitude = location.longitude
-			selectViewModel.setUserPosition(Position(latitude, longitude))
-		}
-	}
-
-	private fun checkLocationPermission() {
-		if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			requestPermissions(
-				arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-				LOCATION_PERMISSION_REQUEST_CODE
-			)
-		}
-	}
-
-	companion object {
-
-		const val LOCATION_PERMISSION_REQUEST_CODE = 100
 	}
 }
+
--- Android/app/src/main/java/com/example/communcationingarden/select/SelectFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/select/SelectFragment.kt
@@ -1,26 +1,52 @@
 package com.example.communcationingarden.select
 
+import android.Manifest
+import android.content.Context
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.communcationingarden.EventObserver
+import com.example.communcationingarden.R
 import com.example.communcationingarden.adapter.GardenListAdapter
 import com.example.communcationingarden.data.Garden
+import com.example.communcationingarden.data.Position
 import com.example.communcationingarden.databinding.FragmentSelectBinding
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class SelectFragment : Fragment() {
 
 	private var _binding: FragmentSelectBinding? = null
 	private val binding get() = _binding!!
-	private val selectViewModel: SelectViewModel by activityViewModels()
+	private val selectViewModel: SelectViewModel by viewModels()
+	private lateinit var locationManager: LocationManager
+	private lateinit var navigationController: NavController
 	private val onClick: () -> Unit = {
 		selectViewModel.openMainScreen()
 	}
 	private val gardenListAdapter = GardenListAdapter(onClick)
+	// TODO:해당 부분 수정
+	private val locationPermissionRequest =
+		registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+			when {
+				permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false),
+				permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+					getUserCurrentLocation()
+				}
+				else -> {
+				}
+			}
+		}
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +60,34 @@ class SelectFragment : Fragment() {
 		super.onViewCreated(view, savedInstanceState)
 		dummyAdapterItem()
 		initView()
+		initObserver()
+		checkLocationPermission()
+	}
+
+	private fun getUserCurrentLocation() {
+		val userLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+		userLocation?.let { location ->
+			val latitude = location.latitude
+			val longitude = location.longitude
+			selectViewModel.setUserPosition(Position(latitude, longitude))
+		}
+	}
+
+	private fun checkLocationPermission() {
+		locationPermissionRequest.launch(
+			arrayOf(
+				Manifest.permission.ACCESS_FINE_LOCATION,
+				Manifest.permission.ACCESS_COARSE_LOCATION
+			)
+		)
+	}
+
+	private fun initObserver() = with(selectViewModel) {
+		mainScreenEvent.observe(viewLifecycleOwner, EventObserver {
+			val userPositionJson = Json.encodeToString(selectViewModel.userPositionLiveData)
+			val bundle = bundleOf("userPosition" to userPositionJson)
+			navigationController.navigate(R.id.infoActivity, bundle)
+		})
 	}
 
 	private fun dummyAdapterItem() {
@@ -56,6 +110,9 @@ class SelectFragment : Fragment() {
 				LinearLayoutManager.VERTICAL
 			)
 		)
+		navigationController = findNavController()
+		locationManager =
+			requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 	}
 
 	override fun onDestroyView() {
