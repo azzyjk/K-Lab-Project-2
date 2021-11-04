5f58aed56f6c03980ebcff6a8dc247b9c037cee7
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-05 01:27:55 +0900

    refactor: LoginRepository 구현체 이름 변경

--- Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepositoryImpl.kt
+++ Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.example.communcationingarden.data.source
 import com.example.communcationingarden.data.source.local.LoginLocalDataSource
 import com.example.communcationingarden.data.source.remote.LoginRemoteDataSource
 
-class DevLoginRepository(
+class LoginRepositoryImpl(
 	private val localDataSource: LoginLocalDataSource,
 	private val remoteDataSource: LoginRemoteDataSource
 ) {
