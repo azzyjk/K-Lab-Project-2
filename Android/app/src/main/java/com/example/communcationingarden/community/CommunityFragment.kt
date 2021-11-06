d7c06f4b8bd4f9bf02d49d3303568ab63e866938
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-06 20:42:05 +0900

    feat(CommunityFragment): RecyclerView 추가

--- Android/app/src/main/java/com/example/communcationingarden/community/CommunityFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/community/CommunityFragment.kt
@@ -1,28 +1,48 @@
 package com.example.communcationingarden.community
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.communcationingarden.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.communcationingarden.adapter.CommunityActivityListAdapter
 import com.example.communcationingarden.databinding.FragmentCommunityBinding
 
 class CommunityFragment : Fragment() {
-
-    private var _binding: FragmentCommunityBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentCommunityBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+	
+	private var _binding: FragmentCommunityBinding? = null
+	private val binding get() = _binding!!
+	private val communityViewModel: CommunityViewModel by viewModels()
+	private val activityListAdapter = CommunityActivityListAdapter()
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentCommunityBinding.inflate(layoutInflater, container, false)
+		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initView()
+		initObserve()
+		communityViewModel.loadActivityList()
+	}
+	
+	private fun initView() = with(binding) {
+		communityActivityRecyclerView.adapter = activityListAdapter
+	}
+	
+	private fun initObserve() = with(communityViewModel) {
+		activityListLiveData.observe(viewLifecycleOwner) { activityList ->
+			activityListAdapter.updateActivityList(activityList)
+		}
+	}
+	
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
 }
--- Android/app/src/main/java/com/example/communcationingarden/community/CommunityViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/community/CommunityViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.communcationingarden.community
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.communcationingarden.data.ActivityInfo
 
 class CommunityViewModel : ViewModel() {
+	
+	private val _activityListLiveData = MutableLiveData<List<ActivityInfo>>()
+	val activityListLiveData get() = _activityListLiveData
+	
+	fun loadActivityList() {
+		_activityListLiveData.value = listOf(
+			ActivityInfo("Hello", "2021-05-05 18:00"),
+			ActivityInfo("Hello", "2021-05-05 18:00"),
+			ActivityInfo("Hello", "2021-05-05 18:00"),
+		)
+	}
 }
