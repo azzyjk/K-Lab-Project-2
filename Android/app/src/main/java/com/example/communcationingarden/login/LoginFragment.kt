e535e5c56523582e8257691bae9c650bfd4f5091
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 01:30:32 +0900

    feat(LoginFragment): SignIn 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/login/LoginFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
@@ -29,6 +30,7 @@ class LoginFragment : Fragment() {
 	): View {
 		_binding = FragmentLoginBinding.inflate(layoutInflater, container, false).apply {
 			viewModel = loginViewModel
+			lifecycleOwner = viewLifecycleOwner
 		}
 		return binding.root
 	}
@@ -45,6 +47,9 @@ class LoginFragment : Fragment() {
 		})
 		selectScreenEvent.observe(viewLifecycleOwner, EventObserver {
 			navigationController.navigate(R.id.selectFragment)
+		})
+		signInFailEvent.observe(viewLifecycleOwner, EventObserver {
+			Toast.makeText(requireContext(), "Failed Sign In...", Toast.LENGTH_SHORT).show()
 		})
 	}
 	
--- Android/app/src/main/java/com/example/communcationingarden/login/LoginViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginViewModel.kt
@@ -3,15 +3,34 @@ package com.example.communcationingarden.login.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.communcationingarden.Event
+import com.example.communcationingarden.data.SignInInfo
 import com.example.communcationingarden.data.source.LoginRepository
+import kotlinx.coroutines.launch
 
-class LoginViewModel(loginRepository: LoginRepository) : ViewModel() {
+class LoginViewModel(val loginRepository: LoginRepository) : ViewModel() {
 	
 	private val _signupScreenEvent = MutableLiveData<Event<Unit>>()
 	val signupScreenEvent: LiveData<Event<Unit>> = _signupScreenEvent
 	private val _selectScreenEvent = MutableLiveData<Event<Unit>>()
 	val selectScreenEvent: LiveData<Event<Unit>> = _selectScreenEvent
+	private val _signInFailEvent = MutableLiveData<Event<Unit>>()
+	val signInFailEvent: LiveData<Event<Unit>> get() = _signInFailEvent
+	val idLiveData = MutableLiveData<String>()
+	val passwordLiveData = MutableLiveData<String>()
+	
+	fun requestSignIn() = viewModelScope.launch {
+		loginRepository.requestSignIn(
+			SignInInfo(
+				idLiveData.value!!,
+				passwordLiveData.value!!
+			)
+		).onSuccess {
+			if (it.name.isEmpty()) _signInFailEvent.value = Event(Unit)
+			else openSelectScreen()
+		}
+	}
 	
 	fun openSignupScreen() {
 		_signupScreenEvent.value = Event(Unit)
