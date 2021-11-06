5a3701cd4a29c04a7b270ef3b992c3958ddb2192
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 04:09:34 +0900

    feat: Retrofit GardenList 받아오는 기능 추가

---
+++ Android/app/src/main/java/com/example/communcationingarden/network/GardenListResponse.kt
@@ -0,0 +1,7 @@
+package com.example.communcationingarden.network
+
+import com.example.communcationingarden.data.GardenInfo
+
+data class GardenListResponse(
+	val gardenList: List<GardenInfo>
+)
--- Android/app/src/main/java/com/example/communcationingarden/network/KLabRetrofitService.kt
+++ Android/app/src/main/java/com/example/communcationingarden/network/KLabRetrofitService.kt
@@ -7,6 +7,10 @@ interface KLabRetrofitService {
 	
 	@POST("/signup")
 	suspend fun requestSignUp(@Body body: HashMap<String, String>): SignUpResponse
+	
 	@POST("/login")
 	suspend fun requestSignIn(@Body body: HashMap<String, String>): SignInResponse
+	
+	@POST("/garden")
+	suspend fun requestGardenList(@Body body: HashMap<String, String>): GardenListResponse
 }
