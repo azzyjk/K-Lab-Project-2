b5dbd18095626f74884a1e67d6f8b8fddcf4d5bc
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 04:10:23 +0900

    refactor: Garden 이름 수정으로 인한 코드 변경

--- Android/app/src/main/java/com/example/communcationingarden/adapter/GardenListAdapter.kt
+++ Android/app/src/main/java/com/example/communcationingarden/adapter/GardenListAdapter.kt
@@ -3,20 +3,20 @@ package com.example.communcationingarden.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.communcationingarden.data.Garden
+import com.example.communcationingarden.data.GardenInfo
 import com.example.communcationingarden.databinding.ItemGardenListBinding
 
 class GardenListAdapter(private val onClick: () -> Unit) :
 	RecyclerView.Adapter<GardenListAdapter.GardenViewHolder>() {
-
-	private var gardenList: List<Garden> = listOf()
-
+	
+	private var gardenList: List<GardenInfo> = listOf()
+	
 	class GardenViewHolder(
 		private val binding: ItemGardenListBinding,
 		private val onClick: () -> Unit
 	) :
 		RecyclerView.ViewHolder(binding.root) {
-
+		
 		init {
 			binding.root.setOnClickListener {
 				binding.garden?.let {
@@ -24,26 +24,26 @@ class GardenListAdapter(private val onClick: () -> Unit) :
 				}
 			}
 		}
-
-		fun setup(garden: Garden) {
+		
+		fun setup(garden: GardenInfo) {
 			binding.garden = garden
 			binding.executePendingBindings()
 		}
 	}
-
+	
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GardenViewHolder {
 		val binding =
 			ItemGardenListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 		return GardenViewHolder(binding, onClick)
 	}
-
+	
 	override fun onBindViewHolder(holder: GardenViewHolder, position: Int) {
 		holder.setup(gardenList[position])
 	}
-
+	
 	override fun getItemCount(): Int = gardenList.size
-
-	fun updateGardenList(newGardenList: List<Garden>) {
+	
+	fun updateGardenList(newGardenList: List<GardenInfo>) {
 		gardenList = newGardenList
 		notifyDataSetChanged()
 	}
