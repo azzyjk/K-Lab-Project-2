da1f452190d46ca5e5ca2799da44aac7d3e071ba
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-06 23:31:48 +0900

    feat(SnsFragment): RecyclerView 추가

--- Android/app/src/main/java/com/example/communcationingarden/sns/SnsFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/sns/SnsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.communcationingarden.adapter.SnsListAdapter
 import com.example.communcationingarden.databinding.FragmentSnsBinding
 
 class SnsFragment : Fragment() {
@@ -13,6 +14,7 @@ class SnsFragment : Fragment() {
 	private var _binding: FragmentSnsBinding? = null
 	private val binding get() = _binding!!
 	private val snsViewModel: SnsViewModel by viewModels()
+	private val snsListAdapter = SnsListAdapter()
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +22,23 @@ class SnsFragment : Fragment() {
 	): View {
 		_binding = FragmentSnsBinding.inflate(layoutInflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initView()
+		initObserve()
+		snsViewModel.loadSnsList()
+	}
+	
+	private fun initView() = with(binding) {
+		snsRecyclerView.adapter = snsListAdapter
+	}
+	
+	private fun initObserve() = with(snsViewModel) {
+		snsInfoListLiveData.observe(viewLifecycleOwner) { snsList ->
+			snsListAdapter.updateSnsInfoList(snsList)
+		}
 	}
 	
 	override fun onDestroyView() {
--- Android/app/src/main/java/com/example/communcationingarden/sns/SnsViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/sns/SnsViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.communcationingarden.sns
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.communcationingarden.data.SnsInfo
 
 class SnsViewModel : ViewModel() {
+	
+	private val _snsInfoListLiveData = MutableLiveData<List<SnsInfo>>()
+	val snsInfoListLiveData get() = _snsInfoListLiveData
+	
+	fun loadSnsList() {
+		_snsInfoListLiveData.value = listOf(
+			SnsInfo("TEST"),
+			SnsInfo("TEST"),
+			SnsInfo("TEST")
+		)
+	}
 }
