a45438f2d39e470b1092b3e71d0d6856fad36990
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 01:16:25 +0900

    feat: LoginRepository SignUp, SignIn 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepository.kt
+++ Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepository.kt
@@ -1,4 +1,13 @@
 package com.example.communcationingarden.data.source
 
+import com.example.communcationingarden.data.SignInInfo
+import com.example.communcationingarden.data.SignUpInfo
+import com.example.communcationingarden.network.SignInResponse
+import com.example.communcationingarden.network.SignUpResponse
+
 interface LoginRepository {
+	
+	suspend fun requestSignUp(signUpInfo: SignUpInfo): Result<SignUpResponse>
+	
+	suspend fun requestSignIn(signInInfo: SignInInfo): Result<SignInResponse>
 }
--- Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepositoryImpl.kt
+++ Android/app/src/main/java/com/example/communcationingarden/data/source/LoginRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.example.communcationingarden.data.source
 
+import com.example.communcationingarden.data.SignInInfo
+import com.example.communcationingarden.data.SignUpInfo
 import com.example.communcationingarden.data.source.local.LoginLocalDataSource
 import com.example.communcationingarden.data.source.remote.LoginRemoteDataSource
+import com.example.communcationingarden.network.SignInResponse
+import com.example.communcationingarden.network.SignUpResponse
 
 class LoginRepositoryImpl(
 	private val localDataSource: LoginLocalDataSource,
 	private val remoteDataSource: LoginRemoteDataSource
 ) : LoginRepository {
+	
+	override suspend fun requestSignUp(signUpInfo: SignUpInfo): Result<SignUpResponse> {
+		return remoteDataSource.requestSignUp(signUpInfo)
+	}
+	
+	override suspend fun requestSignIn(signInInfo: SignInInfo): Result<SignInResponse> {
+		return remoteDataSource.requestSignIn(signInInfo)
+	}
 }
