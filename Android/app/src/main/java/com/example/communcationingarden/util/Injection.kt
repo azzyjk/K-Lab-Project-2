d528421e13b402820d8b9ea3dad7ebc34bf9d76e
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 00:56:07 +0900

    feat(Injection): LoginRemoteDataSource Retrofit 객체 추가

--- Android/app/src/main/java/com/example/communcationingarden/util/Injection.kt
+++ Android/app/src/main/java/com/example/communcationingarden/util/Injection.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import com.example.communcationingarden.data.source.LoginRepositoryImpl
 import com.example.communcationingarden.data.source.local.LoginLocalDataSource
 import com.example.communcationingarden.data.source.remote.LoginRemoteDataSource
+import com.example.communcationingarden.network.RetrofitApi
 
 object Injection {
-
+	
 	fun provideLoginRepository(context: Context): LoginRepositoryImpl {
 		val loginLocalDataSource = provideLoginLocalDataSource(context)
 		val loginRemoteDataSource = provideLoginRemoteDataSource()
 		return LoginRepositoryImpl(loginLocalDataSource, loginRemoteDataSource)
 	}
-
+	
 	private fun provideLoginLocalDataSource(context: Context): LoginLocalDataSource {
 		return LoginLocalDataSource()
 	}
-
+	
 	private fun provideLoginRemoteDataSource(): LoginRemoteDataSource {
-		return LoginRemoteDataSource
+		return LoginRemoteDataSource(RetrofitApi.getKLabRetrofitInstance())
 	}
 }
