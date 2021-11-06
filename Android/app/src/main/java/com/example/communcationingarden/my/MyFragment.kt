a2921250fc76b77e5f55fc78fadb048be60e10b6
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-06 22:46:08 +0900

    feat(MyFragment): 어댑터 정보 넣는 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/my/MyFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/my/MyFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.communcationingarden.adapter.MyActivityListAdapter
 import com.example.communcationingarden.databinding.FragmentMyBinding
 
 class MyFragment : Fragment() {
 	
 	private var _binding: FragmentMyBinding? = null
 	private val binding get() = _binding!!
+	private val myViewModel: MyViewModel by viewModels()
+	private val myActivityListAdapter = MyActivityListAdapter()
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,23 @@ class MyFragment : Fragment() {
 	): View {
 		_binding = FragmentMyBinding.inflate(layoutInflater, container, false)
 		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initView()
+		initObserve()
+		myViewModel.loadMyActivity()
+	}
+	
+	private fun initView() = with(binding) {
+		myActivityRecyclerView.adapter = myActivityListAdapter
+	}
+	
+	private fun initObserve() = with(myViewModel) {
+		myActivityListLiveData.observe(viewLifecycleOwner) { myActivityList ->
+			myActivityListAdapter.updateActivityList(myActivityList)
+		}
 	}
 	
 	override fun onDestroyView() {
--- Android/app/src/main/java/com/example/communcationingarden/my/MyViewModel.kt
+++ Android/app/src/main/java/com/example/communcationingarden/my/MyViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.communcationingarden.my
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.communcationingarden.data.ActivityInfo
 
 class MyViewModel : ViewModel() {
+	
+	private val _myActivityListLiveData = MutableLiveData<List<ActivityInfo>>()
+	val myActivityListLiveData get() = _myActivityListLiveData
+	
+	fun loadMyActivity() {
+		_myActivityListLiveData.value = listOf(ActivityInfo("Hello", "2021-05-05 18:00"))
+	}
 }
