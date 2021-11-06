d53c0e16af92232f84eb2e515ab1d2d577d5a13c
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-06 20:39:54 +0900

    feat: CommunityFragment 네비게이션 추가

--- Android/app/src/main/java/com/example/communcationingarden/info/InfoActivity.kt
+++ Android/app/src/main/java/com/example/communcationingarden/info/InfoActivity.kt
@@ -2,17 +2,30 @@ package com.example.communcationingarden.info
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.communcationingarden.R
 import com.example.communcationingarden.databinding.ActivityInfoBinding
 
 class InfoActivity : AppCompatActivity() {
-
+	
 	private lateinit var binding: ActivityInfoBinding
-
+	private lateinit var navigationController: NavController
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityInfoBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		setSupportActionBar(binding.toolBar)
+		initView()
+	}
+	
+	private fun initView() {
+		val navigationHostFragment =
+			supportFragmentManager.findFragmentById(R.id.info_navHostFragment) as NavHostFragment
+		navigationController = navigationHostFragment.findNavController()
+		binding.bottomNavigation.setupWithNavController(navigationController)
 	}
 }
