057d2eb88aba7e7c7c85b0bacc77e4df139675a5
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-07 17:44:19 +0900

    feat(Retrofit): 유저 활동 목록 가져오는 기능, 활동 나가는 기능 추가

---
+++ Android/app/src/main/java/com/example/communcationingarden/network/ExitResponse.kt
@@ -0,0 +1,5 @@
+package com.example.communcationingarden.network
+
+data class ExitResponse(
+	val result: String
+)
--- Android/app/src/main/java/com/example/communcationingarden/network/InfoRetrofitService.kt
+++ Android/app/src/main/java/com/example/communcationingarden/network/InfoRetrofitService.kt
@@ -10,4 +10,10 @@ interface InfoRetrofitService {
 	
 	@POST("/participate_activity")
 	suspend fun requestParticipateActivity(@Body body: HashMap<String, String>): ParticipateResponse
+	
+	@POST("/activity_user")
+	suspend fun requestUserActivityList(@Body body: HashMap<String, String>): ActivityResponse
+	
+	@POST("/out_activity")
+	suspend fun requestExitActivity(@Body body: HashMap<String, String>): ExitResponse
 }
