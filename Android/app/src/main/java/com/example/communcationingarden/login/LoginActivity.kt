a1e8680a6d94c55cf1d28db1ffa919af33a89e40
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-09 08:29:02 +0900

    feat(LoginActivity): 화면 터치시 키보드 숨기는 기능 추가

--- Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
+++ Android/app/src/main/java/com/example/communcationingarden/login/LoginActivity.kt
@@ -1,19 +1,29 @@
 package com.example.communcationingarden.login
 
+import android.content.Context
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.communcationingarden.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
+	
 	private lateinit var binding: ActivityLoginBinding
 	private lateinit var locationManager: LocationManager
-
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityLoginBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+	}
+	
+	override fun onTouchEvent(event: MotionEvent?): Boolean {
+		val inputMethodManager =
+			getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+		inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+		return true
 	}
 }
 
