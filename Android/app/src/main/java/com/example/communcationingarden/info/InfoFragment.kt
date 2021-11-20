a323562ab06137ced2b7d6ee862df10cf25ca30d
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-20 19:45:21 +0900

    refacor: sns -> info으로 이름 변경

--- Android/app/src/main/java/com/example/communcationingarden/info/InfoFragment.kt
+++ Android/app/src/main/java/com/example/communcationingarden/info/InfoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.communcationingarden.sns
+package com.example.communcationingarden.info
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.communcationingarden.adapter.SnsListAdapter
-import com.example.communcationingarden.databinding.FragmentSnsBinding
+import com.example.communcationingarden.databinding.FragmentInfoBinding
 import com.example.communcationingarden.home.HomeViewModel
 
-class SnsFragment : Fragment() {
+class InfoFragment: Fragment() {
     
-    private var _binding: FragmentSnsBinding? = null
+    private var _binding: FragmentInfoBinding? = null
     private val binding get() = _binding!!
     private val homeViewModel: HomeViewModel by activityViewModels()
     private val snsListAdapter = SnsListAdapter()
@@ -21,7 +21,7 @@ class SnsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSnsBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
     
