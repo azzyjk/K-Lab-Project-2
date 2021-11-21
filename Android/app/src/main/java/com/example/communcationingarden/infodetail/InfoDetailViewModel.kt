30fba2cf5e6ad59e6ad35a2827fc622ede5e0c20
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-11-21 22:47:35 +0900

    feat: InfoDetailFragment 초기 생성

---
+++ Android/app/src/main/java/com/example/communcationingarden/infodetail/InfoDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.communcationingarden.infodetail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InfoDetailViewModel @Inject constructor(
+
+): ViewModel() {
+}
